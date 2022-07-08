ce4f42fd6a70fa0863e46f5934f0eae9079946e5
Shengfu Cheng <3215656080@qq.com>
2022-07-08 17:02:56 +0800

    Simulate the transmission matrix

---
+++ generate_tm.m
@@ -0,0 +1,31 @@
+
+function TM = generate_tm(M, N)
+    %generates a Transmission Matrix (TM) of a scattering medium
+    % M: height of the TM
+    % N: weight of the TM
+      
+    sz_grains = 4;   %size of a single speckle grain
+    if mod(ceil(sqrt(M) / sz_grains), 2) == 0
+        n_grains = ceil(sqrt(M) / sz_grains); %number of speckle grains
+    else
+        n_grains = ceil(sqrt(M) / sz_grains)+1; 
+    end  
+         
+    [Y, X] = meshgrid(1:n_grains, 1:n_grains);
+    pupil = (X-n_grains/2).^2 + (Y-n_grains/2).^2 < (n_grains/2)^2; %pupil definition 
+    
+    % A speckle is modeled by a random phase in the Fourier space
+    bruit = exp(2*1i*pi * rand(n_grains, n_grains, N, 'single')); 
+    bruit = bruit .* single(pupil);              %imaging system CTF/pupil func
+    
+    % Fourier transform to go in the object space with zero padding 
+    TM = zeros(M, N,  'single');
+    [Y, X] = meshgrid(1:sqrt(M), 1:sqrt(M));
+    mask = ((X - sqrt(M)/2).^2 + (Y - sqrt(M)/2).^2) < (sqrt(M)/2)^2;
+
+    for j = 1:N
+        Temp = fft2(fftshift(padarray(bruit(:, :, j), ...
+            [sqrt(M)/2 - n_grains/2, sqrt(M)/2 - n_grains/2]))).* mask; 
+        TM(:, j) = 1/M * Temp(:); 
+    end
+end
